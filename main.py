a1efd53b1b01ed669b761b7e4a645490cedb4c75
myriam metenier <myriammetenier@yahoo.fr>
2021-09-28 19:49:56 +0200

    modified:   main.py

--- main.py
+++ main.py
@@ -4,35 +4,73 @@ import sys
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    rep0 = input("Outils du disque, réseau où sécurité continué ? o|n:")
+    rep0 = input("Outils Ordinateur, Disque, Réseau où Sécurité voulez-vous continué ? o|n:")
     while rep0 == "o":
         rep0 = input("Outils du disque, réseau où sécurité continué ? o|n:")
         if rep0 == "o":
-            choix = input("Outils du disque, réseau où sécurité ? 1|2|3 :")
+            choix = input("Outils Ordinateur, Disque, Réseau où Sécurité ? 1|2|3|4 :")
             if choix == "1":
+                print_Ordi(name)
+            if choix == "2":
                 print_Di(name)
-            elif choix == "2":
-                print_R(name)
             elif choix == "3":
+                print_R(name)
+            elif choix == "4":
                 print_S(name)
         elif rep0 == "n":
             sys.exit()
 
+
+def print_Ordi(name):
+    ordi0 = input("Administration ordinateur 1, Surveillance ordinateur 2, Analyse ordinateur 3 ? 1|2|3|4 :")
+    if ordi0 == "1":
+        ordi = input("Ajouter compte , Moniteur ressource, Analyse virus, Access Distance 4 ? 1|2|3|4 :")
+        if ordi == "1":
+            os.system("netplwiz")
+        elif ordi == "2":
+            os.system("resmon")
+        elif ordi == "3":
+            os.system("mrt")
+        elif ordi == "4":
+            os.system("mstsc")
+
+    if ordi0 == "2":
+        ordi = input("Check Disk 1, Check NTFS 2,Defragment 3 ? 1|2|3:")
+        if ordi == "1":
+            os.system("sfc.exe")
+        elif ordi == "2":
+            os.system("CHKNTFS c:")
+        elif ordi == "3":
+            os.system("DEFRAG")
+
+    elif ordi0 == "3":
+        ordi = input("Restauration 1, Partition 2, Propriété System 3 ? 1|2|3 :")
+        if ordi == "1":
+            os.system("strui.exe")
+
+        elif ordi == "2":
+            os.system("DISKPART")
+        elif ordi == "3":
+            os.system("sysdm.cpl")
+
+
 def print_Di(name):
     disc0 = input ("Observation disque, Analyse disque, restauration ? 1|2|3 :")
     if disc0 == "1":
-        disc = input("Observateur d'événements 1, Propriétés système 2, Registre 3 ? 1|2|3 :")
+        disc = input("Observateur d'événements 1, Propriétés système 2, Registre 3, Service 4 ? 1|2|3|4 :")
         if disc == "1":
             os.system("Eventvwr.msc")
         elif disc == "2":
             os.system("sysdm.cpl")
         elif disc == "3":
             os.system("Regedit.exe")
+        elif disc == "4":
+            os.system("services.msc")
 
     elif disc0 == "2":
         disc = input("Check Disk 1, Check NTFS 2,Defragment 3 ? 1|2|3:")
         if disc == "1":
-            os.system("CHKDSK")
+            os.ctermid("CHKDSK")
         elif disc == "2":
             os.system("CHKNTFS c:")
         elif disc == "3":
