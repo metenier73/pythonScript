b0e931f6fe85b4cd353640dd148f2bd207e3a7cf
myriam metenier <myriammetenier@yahoo.fr>
2021-09-29 17:46:45 +0200

    new file:   nppBackup/README.md.2021-09-29_170442.bak 	new file:   setup.py 	nppBackup/README.md.2021-09-29_174140.bak 	nppBackup/README.md.2021-09-29_174200.bak 	nppBackup/README.md.2021-09-29_174226.bak

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='main.py',
+    version='10',
+    packages=[''],
+    url='https://github.com/metenier73/pythonScript.git',
+    license='opensource',
+    author='metenier',
+    author_email='meteniermyriam@yahoo.fr',
+    description='Boite à outils windows 10'
+)
